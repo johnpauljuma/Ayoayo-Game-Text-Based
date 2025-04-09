492978ac14c774174304b8045e3dcf2799517de8
johnpauljuma <johnpauljuma29@gmail.com>
2025-04-09 16:24:33 +0300

    added main class and reflection.txt files.

--- src/main/java/org/ayoayo/Main.java
+++ src/main/java/org/ayoayo/Main.java
@@ -1,17 +1,19 @@
 package org.ayoayo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Ayoayo game = new Ayoayo();
+        Player player1 = game.createPlayer("Jensen");
+        Player player2 = game.createPlayer("Brian");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(game.playGame(1, 3));
+        game.playGame(1, 1);
+        game.playGame(1, 2);
+        game.playGame(1, 3);
+        game.playGame(1, 4);
+        game.playGame(1, 5);
+        game.playGame(1, 6);
+        game.printBoard();
+        System.out.println(game.returnWinner());
     }
 }
